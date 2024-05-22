95244c59277160665232c8ca969cc27e6acff687
Brandon Winger-Air <brandonwinger-air@outlook.com>
2024-05-22 16:31:19 -0400

    counter style and computed property array

--- src/Counter.vue
+++ src/Counter.vue
@@ -4,27 +4,43 @@
             Button
         </button>
         <div>{{ counter }}</div>
+        <div>{{ arrayOfStars }}</div>
     </div>
 </template>
 
 <script>
-import { ref, onMounted } from 'vue'
+import { ref, onMounted, watch, computed } from 'vue'
 
 export default {
     props: {
-        emoji: { type: String, default: '⭐' }
+        star: { type: String, default: '⭐' }
     },
     setup(props) {
-        console.log(props.emoji)
+        console.log(props.star)
         
         onMounted(() => {
             console.log(`Counter component has been mounted!`)
         })
 
-        const counter = ref(0)
+        const counter = ref(1)
         const increment = () => counter.value++
 
-        return { counter, increment }
+        watch(counter, current => {
+            if (current === 5) console.log('You have clicked five times!');
+        })
+
+        const arrayOfStars = computed(() =>
+            Array.from(new Array(counter.value), () => props.star).join(' ')
+        )
+
+        return { counter, increment, arrayOfStars }
     }
 }
 </script>
+
+<style>
+div {
+    text-align: center;
+    width: 200px;
+}
+</style>
