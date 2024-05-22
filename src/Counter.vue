5a63010767c12d9c3a044b80cd011149a8616eff
Brandon Winger-Air <brandonwinger-air@outlook.com>
2024-05-22 16:13:14 -0400

    counter ref variable and increment functionality

--- src/Counter.vue
+++ src/Counter.vue
@@ -1,24 +1,30 @@
 <template>
     <div>
-        <button>
+        <button @click="increment">
             Button
         </button>
+        <div>{{ counter }}</div>
     </div>
 </template>
 
 <script>
-import { onMounted } from 'vue'
+import { ref, onMounted } from 'vue'
 
 export default {
     props: {
         emoji: { type: String, default: '⭐' }
     },
     setup(props) {
-        console.log(props.emoji);
+        console.log(props.emoji)
         
         onMounted(() => {
-            console.log(`Counter component has been mounted!`);
+            console.log(`Counter component has been mounted!`)
         })
+
+        const counter = ref(0)
+        const increment = () => counter.value++
+
+        return { counter, increment }
     }
 }
 </script>
